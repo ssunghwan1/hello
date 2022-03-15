47e425e1be234c09d45ab10a794892fc581ef51a
CSH <ssunghwan1@gmail.com>
2022-03-15 14:28:33 +0900

    ✨ comma

---
+++ src/main/java/com/okdevtv/tdd/hello/StringUtils.java
@@ -0,0 +1,14 @@
+package com.okdevtv.tdd.hello;
+
+import java.text.NumberFormat;
+import java.util.Locale;
+
+public class StringUtils {
+
+	public static String comma(int num) {
+		String string = NumberFormat.getNumberInstance(Locale.US).format(num);
+		
+		return string;
+	}
+
+}
