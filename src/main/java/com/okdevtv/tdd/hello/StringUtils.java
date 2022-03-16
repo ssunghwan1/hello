61fc5100f15544a7753db35457a92714cd9a0a1d
CSH <ssunghwan1@gmail.com>
2022-03-16 15:04:57 +0900

    🐛 code smell #5

--- src/main/java/com/okdevtv/tdd/hello/StringUtils.java
+++ src/main/java/com/okdevtv/tdd/hello/StringUtils.java
@@ -6,9 +6,7 @@ import java.util.Locale;
 public class StringUtils {
 
 	public static String comma(int num) {
-		String string = NumberFormat.getNumberInstance(Locale.US).format(num);
-		
-		return string;
+		return NumberFormat.getNumberInstance(Locale.US).format(num);
 	}
 
 }
